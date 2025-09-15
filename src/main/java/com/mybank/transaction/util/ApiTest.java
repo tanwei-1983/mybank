cb3b309b4f7670bea362590dfcf67d27bfdbc80d
tanwei-1983 <50787443+tanwei-1983@users.noreply.github.com>
2025-09-15 17:54:33 +0800

    Update ApiTest.java

--- src/main/java/com/mybank/transaction/util/ApiTest.java
+++ src/main/java/com/mybank/transaction/util/ApiTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
